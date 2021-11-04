045b11c2e56a567ad68c4d046a12ea55a8c1af46
bobacrack <bobacrack>
2021-11-04 13:52:02 +0100

    extracted csv files

--- NaturalLanguageProcessing/main.py
+++ NaturalLanguageProcessing/main.py
@@ -1,12 +1,24 @@
 import csv
 from words import ENGLISH_STOP_WORDS
 from sklearn.feature_extraction.text import CountVectorizer
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import PorterStemmer
 from nltk import WordNetLemmatizer
 
-illegal = ['and','zombie']
+porter = PorterStemmer()
+lemma = WordNetLemmatizer()
+
+def stemSentence(sentence):
+    token_words=word_tokenize(sentence)
+    stem_sentence=[]
+    for word in token_words:
+        stem_sentence.append(lemma.lemmatize(word))
+        stem_sentence.append(" ")
+    return "".join(stem_sentence)
 
 def write(result):
-    with open("NaturalLanguageProcessing/data/vector.csv", "w", encoding="UTF-8") as f:
+    with open("NaturalLanguageProcessing/data/F500_N22_Lemma.csv", "w", encoding="UTF-8") as f:
         writer = csv.writer(f)
         writer.writerow(result)
 
@@ -18,7 +30,9 @@ def prepData():
         for line in fin.readlines():
             text = line.strip()
             listDoc = text.split(",")
-            reviews.append(listDoc[0].replace("'", ""))        
+            listDoc[0].replace("'", "")
+            listDoc[0] = stemSentence(listDoc[0])
+            reviews.append(listDoc[0])
             res.append(listDoc[1])
 
     del reviews[0]
@@ -26,12 +40,13 @@ def prepData():
     return reviews, res
 
 
+
+
 reviews, result = prepData()
-countVectorizer = CountVectorizer(analyzer='word', max_features=200, stop_words=ENGLISH_STOP_WORDS)
+countVectorizer = CountVectorizer(analyzer='word', max_features=5000, stop_words=ENGLISH_STOP_WORDS, ngram_range=(2,2))
 X = countVectorizer.fit_transform(reviews)
-#print(X)
 print(countVectorizer.get_feature_names_out())
 result = X.toarray()
-#write(result)
+write(result)
 #print(result)
 
